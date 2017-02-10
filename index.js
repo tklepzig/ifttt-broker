92df36b34cad7a73de5b5762b3e6652e062953a5
Thomas Klepzig <thomas.klepzig@comparex.de>
2017-02-10 14:59:31 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
+
+const port = 8080;
+const app = express();
+const httpServer = http.Server(app);
+const socketIoServer = socketIo(httpServer, {
+    pingTimeout: 2000,
+    pingInterval: 2000
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//test with curl: curl -H "Content-Type: application/json" -d "{\"msg\": 21}" http://localhost:[port]
+app.post("/", (req, res) => {
+    socketIoServer.sockets.emit("message", req.body.msg);
+});
+
+socketIoServer.on('connection', function (socket) {
+    let clientIp = socket.request.connection.remoteAddress;
+    console.log('Client connected:\t' + clientIp);
+});
+
+httpServer.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
